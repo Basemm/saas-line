23f697c9012fe2c3d511421888570ebdb86c5510
Basem Mostafa <basm.egy@gmail.com>
2024-06-11 11:51:58 +0300

    - feat: add next-bundle analyzer

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+import NextBundleAnalyzer from '@next/bundle-analyzer'
+
 const nextConfig = {}
 
-export default nextConfig
+const withBundleAnalyzer = NextBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+export default process.env.ANALYZE === 'true'
+  ? withBundleAnalyzer(nextConfig)
+  : nextConfig

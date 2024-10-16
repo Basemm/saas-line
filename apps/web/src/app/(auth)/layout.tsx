8fa7c83e9c72faf29768932bd62af210886b54d9
Basem Mostafa <basm.egy@gmail.com>
2024-10-16 17:23:19 +0300

    - fix: remove unnecessary vertical scroll

--- apps/web/src/app/(auth)/layout.tsx
+++ apps/web/src/app/(auth)/layout.tsx
@@ -15,10 +15,7 @@ export default function RootLayout({
   return (
     <html className="dark antialiased" lang="en">
       <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable
-        )}
+        className={cn('bg-background font-sans antialiased', fontSans.variable)}
       >
         {children}
       </body>

a3c7e8c5c02094d1cfbebb87051c5d033ee99643
Basem Mostafa <basm.egy@gmail.com>
2024-10-16 17:24:31 +0300

    - feat: add logo to login page

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -2,6 +2,7 @@ import { Button } from '@repo/core-ui/components/ui/button'
 import { Input } from '@repo/core-ui/components/ui/input'
 import { Label } from '@repo/core-ui/components/ui/label'
 import { Separator } from '@repo/core-ui/components/ui/separator'
+import { RocketIcon } from 'lucide-react'
 import type { Metadata } from 'next'
 import Link from 'next/link'
 
@@ -12,7 +13,16 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="flex h-screen">
+    <main className="mt-6 flex flex-col gap-24">
+      <div className="flex justify-center">
+        <Link
+          className="flex items-center gap-2 font-semibold md:text-base"
+          href="/"
+        >
+          <RocketIcon className="h-8 w-8" />
+          <h1 className="px-1 text-4xl">SaaS Line</h1>
+        </Link>
+      </div>
       <div className="mx-auto w-96 max-w-sm space-y-6 self-center">
         <div className="space-y-2 text-center">
           <h1 className="text-3xl font-bold">Login</h1>
@@ -87,7 +97,7 @@ export default function Home() {
         </div>
         <div className="text-center text-sm">
           Don&apos;t have an account?{' '}
-          <Link className="underline" href="/signup">
+          <Link className="underline" href="/sign-up">
             Sign up
           </Link>
         </div>

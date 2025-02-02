92998ca8c2a3e2d939b6fd258bef77810a4e1964
Basem Mostafa <basm.egy@gmail.com>
2025-02-02 11:40:14 +0200

    - feat: add continue with google button in login/signup form

---
+++ apps/web/src/app/auth/_components/continueWithGoogleButton.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import type { StaticNotificationData } from '@/components/staticNotification'
+import getConfig from '@/lib/getConfig'
+import getSupabaseBrowserClient from '@/lib/getSupabaseBrowserClient'
+import Script from 'next/script'
+import { useEffect } from 'react'
+
+declare global {
+  interface Window {
+    loginWithGoogleCallback: (response: { credential: string }) => Promise<void>
+  }
+}
+
+interface ContinueWithGoogleButtonProps {
+  onError: (errorDetails: StaticNotificationData) => void
+}
+
+export default function ContinueWithGoogleButton(
+  props: ContinueWithGoogleButtonProps
+) {
+  const { onError } = props
+  useEffect(() => {
+    window.loginWithGoogleCallback = async (response: {
+      credential: string
+    }) => {
+      const { error } = await getSupabaseBrowserClient().auth.signInWithIdToken(
+        {
+          provider: 'google',
+          token: response.credential,
+        }
+      )
+
+      if (error) {
+        onError({
+          type: 'error',
+          title: 'Failed',
+          message: 'Cannot login with Google at the time.',
+        })
+        console.error('Continue with Google button error', error)
+        return
+      }
+
+      window.location.reload()
+    }
+  }, [onError])
+
+  return (
+    <>
+      <Script async src="https://accounts.google.com/gsi/client" />
+      <div
+        data-auto_prompt="false"
+        data-callback="loginWithGoogleCallback"
+        data-client_id={getConfig().googleOAuthClientId}
+        data-context="signin"
+        data-use_fedcm_for_prompt="true"
+        data-ux_mode="popup"
+        id="g_id_onload"
+      />
+
+      <div
+        className="g_id_signin"
+        data-locale="en-US"
+        data-logo_alignment="left"
+        data-shape="pill"
+        data-size="large"
+        data-text="continue_with"
+        data-theme="outline"
+        data-type="standard"
+      />
+    </>
+  )
+}
--- apps/web/src/lib/getConfig.ts
+++ apps/web/src/lib/getConfig.ts
@@ -5,11 +5,14 @@ const configSchema = z.object({
   NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
   // Arbitrary length, it's mostly larger than 50 to ensure no random/test text
   NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(10),
+  NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID: z.string().min(10),
 })
 
 const config = {
   NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
   NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID:
+    process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID,
 }
 
 function getConfig() {
@@ -23,6 +26,7 @@ function getConfig() {
   return {
     supabaseURL: result.data.NEXT_PUBLIC_SUPABASE_URL,
     supabaseAnonKey: result.data.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    googleOAuthClientId: result.data.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID,
   }
 }
 

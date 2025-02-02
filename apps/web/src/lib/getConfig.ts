2f96363e865e4cb9cbedbdc135dc3e948eaf91b6
Basem Mostafa <basm.egy@gmail.com>
2025-02-02 11:47:58 +0200

    - refactor: simplify getConfig code to avoid repetition of env variables

--- apps/web/src/lib/getConfig.ts
+++ apps/web/src/lib/getConfig.ts
@@ -2,17 +2,16 @@ import memoize from '@repo/utils/memoize'
 import { z } from 'zod'
 
 const configSchema = z.object({
-  NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+  supabaseURL: z.string().url(),
   // Arbitrary length, it's mostly larger than 50 to ensure no random/test text
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(10),
-  NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID: z.string().min(10),
+  supabaseAnonKey: z.string().min(10),
+  googleOAuthClientId: z.string().min(10),
 })
 
 const config = {
-  NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID:
-    process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID,
+  supabaseURL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  googleOAuthClientId: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID,
 }
 
 function getConfig() {
@@ -23,11 +22,7 @@ function getConfig() {
     throw new Error(result.error.errors.join('\n'))
   }
 
-  return {
-    supabaseURL: result.data.NEXT_PUBLIC_SUPABASE_URL,
-    supabaseAnonKey: result.data.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    googleOAuthClientId: result.data.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID,
-  }
+  return result.data
 }
 
 export default memoize(getConfig)
